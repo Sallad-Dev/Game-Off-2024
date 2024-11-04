09b56ca4aca51f40431091946103792d8e826f8a
Sallad-Dev <165468680+Sallad-Dev@users.noreply.github.com>
2024-11-04 17:54:39 +1000

    gun basics implemented
    raycasting method deletes a box in test world which is good.

--- Charcaters/Player/scense and scripts/AgentJetStream.gd
+++ Charcaters/Player/scense and scripts/AgentJetStream.gd
@@ -7,7 +7,22 @@ extends CharacterBody2D
 
 
 func _physics_process(_delta: float) -> void:
+	#raycast shenanigans
+	var space_state = get_world_2d().direct_space_state
+	# use global coordinates, not local to node
+	var query = PhysicsRayQueryParameters2D.create(global_position, get_global_mouse_position())
+	var result = space_state.intersect_ray(query)
+	
+	if result:
+		var obj = result.collider
+		if Input.is_action_just_pressed("action_shoot"):
+			obj.gamer()
+	
 
+		##push boxes test
+		#obj.global_position += (obj.global_position - global_position).limit_length(5)
+		#print("Hit at point: ", obj)
+		
 #movement inputs (basic and can be modified to be more robust)
 
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
---
+++ TestWorlds/static_body_2d.gd
@@ -0,0 +1,5 @@
+extends StaticBody2D
+
+func gamer():
+	print("im a gamer")
+	queue_free()
